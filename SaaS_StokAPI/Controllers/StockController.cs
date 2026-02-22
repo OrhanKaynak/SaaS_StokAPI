c9b6199dc839ef05de3a552720f2c1d8ce9ac9a4
OrhanKaynak <kaynakorhan354@gmail.com>
2026-02-23 01:03:16 +0300

    23-02-26 CRUD

--- SaaS_StokAPI/Controllers/StockController.cs
+++ SaaS_StokAPI/Controllers/StockController.cs
@@ -9,24 +9,31 @@ namespace SaaS_StokAPI.Controllers
 
     public class StockController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public StockController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
-        public IActionResult GetAllStocks()
+        public async Task<IActionResult> GetAllStocks()
         {
             var realStocks = new List<Product>();
 
-            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=SaasDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                connection.Open();
+                await connection.OpenAsync();
 
                 string sqlQuery = "SELECT Id, Name, StockQuantity, Price FROM Stocks";
 
                 using (SqlCommand command = new SqlCommand(sqlQuery, connection))
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
                     {
-                        while (reader.Read())
+                        while (await reader.ReadAsync())
                         {
                             var product = new Product
                             {
@@ -44,13 +51,13 @@ namespace SaaS_StokAPI.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddStock([FromBody] Product newProduct)
+        public async Task<IActionResult> AddStock([FromBody] Product newProduct)
         {
-            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=SaasDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                connection.Open();
+                await connection.OpenAsync();
                 string sqlQuery = "INSERT INTO Stocks (Name, StockQuantity, Price) VALUES (@Name, @Quantity, @Price)";
 
                 using (SqlCommand command = new SqlCommand(@sqlQuery, connection))
@@ -59,10 +66,64 @@ namespace SaaS_StokAPI.Controllers
                     command.Parameters.AddWithValue("Quantity", newProduct.StockQuantity);
                     command.Parameters.AddWithValue("Price", newProduct.Price);
 
-                    command.ExecuteNonQuery();
+                    await command.ExecuteNonQueryAsync();
                 }
             }
             return Ok("Product Added");
+        }
+
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> UpdadteStock(int id, [FromBody] Product updatedProduct)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string sqlQuery = "UPDATE Stocks SET Name = @Name, StockQuantity = @Quantity, Price = @Price WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(@sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Name", updatedProduct.Name);
+                    command.Parameters.AddWithValue("@Quantity", updatedProduct.StockQuantity);
+                    command.Parameters.AddWithValue("@Price", updatedProduct.Price);
+
+                    int affectedRows = await command.ExecuteNonQueryAsync();
+
+                    if (affectedRows == 0)
+                    {
+                        return NotFound("There is no product with this mussel in the stocks");
+                    }
+                }
+            }
+            return Ok("Product successfuly updated");
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> DeleteStock(int id)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                string sqlQuery = "DELETE FROM Stocks WHERE Id = @Id";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    int affectedRows = await command.ExecuteNonQueryAsync();
+
+                    if(affectedRows == 0)
+                    {
+                        return NotFound("Product couldn't found.");
+                    }
+                }
+            }
+            return Ok("Produvt succsessfully deleted.");
         }
     }
 }
